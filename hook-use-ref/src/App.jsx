58fe5aad92a109ce2b67ad8a4a8913867e7aeb5c
lucasjaviluna <lucasjaviluna@gmail.com>
2026-07-13 22:23:40 -0300

    video

--- hook-use-ref/src/App.jsx
+++ hook-use-ref/src/App.jsx
@@ -2,8 +2,7 @@ import './App.css'
 import VideoPlayer from './VideoPlayer'
 
 function App() {
-  // const videoSource = 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4';
-  const videoSource = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'
+  const videoSource = 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4'
 
   return (
     <>
